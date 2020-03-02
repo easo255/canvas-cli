b5e96e6bb7f085c316c61c18f249dc4bc0c9571b
Edis <edis.asotic@gmail.com>
2020-03-02 13:49:39 +0100

    :heavy_plus_sign: Added flag --any for getting any unread announcements

--- src/cmds/announcements.js
+++ src/cmds/announcements.js
@@ -32,43 +32,65 @@ module.exports = (args) => {
         spinner.stop();
         return result;
       }
-      
-      fetchData().then( result => {
-        inquirer
-            .prompt([
-              {
-                type:'list',
-                name:'course_id',
-                message:'Select subject:',
-                choices:result
-              }
-            ])
-            .then(answers => {
-              let filter = 'unread';
-              if(args.all){
-                filter = 'all';
-              }
 
-              axios.get(`/api/v1/courses/${answers.course_id}/discussion_topics?only_announcements=true&filter_by=${filter}`)  
-              .then((response) => {
-                response.sort((a,b) => (a.created_at > b.created_at) ? 1 : ((b.created_at > a.created_at) ? -1 : 0));
-                if(response.length > 0 ){
-                  response.forEach(announcement =>{
-                    console.log('Posted: ', new Date(announcement.created_at).toLocaleDateString() + ' by ' +  announcement.author.display_name)
-                    console.log('Title: ', announcement.title)
-                    console.log('------------------------------')
-                    console.log('Message: ', striptags(announcement.message))
-                    console.log('------------------------------')
-                    if(announcement.read_state == 'unread'){
-                      axios.put(`/api/v1/courses/${answers.course_id}/discussion_topics/${announcement.id}/read`)
-                    }
-                  })
-                }else{
-                  console.log("No unread announcements")
+      if(args.any){
+        fetchData().then(courses =>{
+          var contextCodes = [];
+          courses.forEach(course =>{
+            contextCodes.push(`&context_codes[]=course_${course.value}`)
+          })
+          axios.get(`/api/v1/announcements?${contextCodes.join()}`).then((response)=>{
+              response.forEach(announcement =>{
+                if(announcement.read_state == 'unread'){
+                      console.log('Posted: ', new Date(announcement.created_at).toLocaleDateString() + ' by ' +  announcement.author.display_name)
+                      console.log('Title: ', announcement.title)
+                      console.log('------------------------------')
+                      console.log('Message: ', striptags(announcement.message))
+                      console.log('*******************************')
                 }
+              })
+          })
+        })
 
-                  }).catch();
-            });}
-      );
+      }else{
+        fetchData().then( result => {
+          inquirer
+              .prompt([
+                {
+                  type:'list',
+                  name:'course_id',
+                  message:'Select subject:',
+                  choices:result
+                }
+              ])
+              .then(answers => {
+                let filter = 'unread';
+                if(args.all){
+                  filter = 'all';
+                }
+  
+                axios.get(`/api/v1/courses/${answers.course_id}/discussion_topics?only_announcements=true&filter_by=${filter}`)  
+                .then((response) => {
+                  response.sort((a,b) => (a.created_at > b.created_at) ? 1 : ((b.created_at > a.created_at) ? -1 : 0));
+                  if(response.length > 0 ){
+                    response.forEach(announcement =>{
+                      console.log('Posted: ', new Date(announcement.created_at).toLocaleDateString() + ' by ' +  announcement.author.display_name)
+                      console.log('Title: ', announcement.title)
+                      console.log('------------------------------')
+                      console.log('Message: ', striptags(announcement.message))
+                      console.log('*******************************')
+                      if(announcement.read_state == 'unread'){
+                        axios.put(`/api/v1/courses/${answers.course_id}/discussion_topics/${announcement.id}/read`)
+                      }
+                    })
+                  }else{
+                    console.log("No unread announcements")
+                  }
+  
+                    }).catch();
+              });}
+        );
+      }
+      
 
 }
