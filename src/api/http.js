76105a17d8fb234f2d7e649c17a204fb31d87d5d
Herman Stoud Platou <hsplatou@gmail.com>
2020-03-05 22:15:09 +0100

    wip

--- src/api/http.js
+++ src/api/http.js
@@ -1,53 +1,57 @@
-const axios = require('axios');
-const configData = require('../config')
-      
-function parseError (messages) {
-    // error
-    if (messages) {
-      if (messages instanceof Array) {
-        return Promise.reject({ messages: messages })
-      } else {
-        return Promise.reject({ messages: [messages] })
-      }
-    } 
-  }
-  
-  function parseBody (response) {
-    //  if (response.status === 200 && response.data.status.code === 200) { // - if use custom status code
-     if (response.status === 200) {    
-      return response.data
-    } else {
-      return parseError(response.data.messages)
-    }
-  }
+const axios = require("axios");
+const configData = require("../config");
 
-  let instance = axios.create({
-    baseURL: `${configData.baseURL}`
-  })
-  instance.interceptors.request.use((config) => {
-    config. headers= { Authorization: `Bearer ${configData.Token}` } 
-    return config
-  }, error => {
-    return Promise.reject(error)
-  })
-  
-  instance.interceptors.response.use((response) => {
-      return parseBody(response)
-  }, error => {
-    console.warn('Error status', error.response.status)
-    if (error.response) {
-      return parseError(error.response.data)
+function authenticate() {
+  /* validate connection */
+  console.log("autenticate");
+}
+
+function parseError(messages) {
+  // error
+  if (messages) {
+    if (messages instanceof Array) {
+      return Promise.reject({ messages: messages });
     } else {
-      return Promise.reject(error)
+      return Promise.reject({ messages: [messages] });
     }
-  })
- 
-  exports.instance = instance;
-  
-  
-   
-  
-   
-  
-  
-   
+  }
+}
+
+function parseBody(response) {
+  //  if (response.status === 200 && response.data.status.code === 200) { // - if use custom status code
+  if (response.status === 200) {
+    return response.data;
+  } else {
+    return parseError(response.data.messages);
+  }
+}
+
+let instance = axios.create({
+  baseURL: `${configData.baseURL}`
+});
+instance.interceptors.request.use(
+  config => {
+    config.headers = { Authorization: `Bearer ${configData.Token}` };
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  response => {
+    return parseBody(response);
+  },
+  error => {
+    console.warn("Error status", error.response.status);
+    if (error.response) {
+      return parseError(error.response.data);
+    } else {
+      return Promise.reject(error);
+    }
+  }
+);
+
+exports.instance = instance;
+exports.authenticate = authenticate;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,58 @@
-const minimist = require('minimist')
+const minimist = require("minimist");
+const authenticate = require("../src/api/http").authenticate;
+const fs = require("fs");
+const path = require("path");
+
+const verifyConfig = () => {
+  filePath = path.join(__dirname, "../src/config.json");
+  fs.readFile(filePath, { encoding: "utf-8" }, function(err, data) {
+    if (!err) {
+      console.log("found file. Verifying cridentials");
+    } else {
+      console.log("cannot find config.json");
+    }
+  });
+};
 
 module.exports = () => {
-  const args = minimist(process.argv.slice(2))
-  let cmd = args._[0] || 'help'
+  const args = minimist(process.argv.slice(2));
+  /*check if user has authenticated */
+
+  verifyConfig();
+
+  let cmd = args._[0] || "help";
 
   if (args.version || args.v) {
-    cmd = 'version'
+    cmd = "version";
   }
 
   if (args.help || args.h) {
-    cmd = 'help'
+    cmd = "help";
   }
 
   switch (cmd) {
-    case 'messages':
-      require('./cmds/messages')(args)
-      break
+    case "messages":
+      require("./cmds/messages")(args);
+      break;
 
-      case 'help':
-      require('./cmds/help')(args)
-      break
+    case "help":
+      require("./cmds/help")(args);
+      break;
 
-      case 'version':
-      require('./cmds/version')(args)
-      break
+    case "version":
+      require("./cmds/version")(args);
+      break;
 
-      case 'assignments':
-      require('./cmds/assignments')(args)
-      break
+    case "assignments":
+      require("./cmds/assignments")(args);
+      break;
 
-      case 'announcements':
-      require('./cmds/announcements')(args)
-      break
+    case "announcements":
+      require("./cmds/announcements")(args);
+      break;
 
-
-      
     default:
-      console.error(`"${cmd}" is not a valid command!`)
-      break
+      console.error(`"${cmd}" is not a valid command!`);
+      break;
   }
-}
+};
